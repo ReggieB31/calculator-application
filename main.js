e619b2e7a2517ddec218e12765cbbfec65739f23
Reginald BIllings <billingsreginald3@gmail.com>
2026-06-23 22:17:30 -0400

    git push -A

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
 let inputDisplay = "";
 let display = document.getElementById("input");
-let var1;
-let var2;
+let var1 = "";
+let var2 = "";
 let counter = 0;
+let operationCounter = 0;
 let operation;
 
 function calculate(e){
-    if(counter == 0 && !NaN(e.target.dataset.value)){
-        let var1 = e.target.dataset.value;
-        counter++; 
-    } else if(counter == 1 && !NaN(e.target.dataset.value)) {
-        var2 = e.target.dataset.value;
-        counter++;
+    let inputValue = e.target.dataset.value;
+    if(counter == 0 && !isNaN(inputValue)){
+        var1 += inputValue; 
+    } else if(counter == 1 && !isNaN(inputValue)) {
+        var2 += inputValue;
     } 
     
-    if(counter == 0 || operation != undefined){
-        
+    if(isNaN(inputValue) && inputValue != "clear"){
+        if(operationCounter < 1){
+        operation = inputValue;
+        inputDisplay += inputValue;
+        display.textContent = inputDisplay;
+        operationCounter++;
+        counter++;
     }
-    inputDisplay += e.target.dataset.value;
-    display.textContent = inputDisplay;
-    
+}
+
+    if(counter < 2 && !isNaN(inputValue)){
+        inputDisplay += inputValue;
+        display.textContent = inputDisplay;
+    } 
+    if(inputValue == "="){
+        let value;
+        let num1 = Number(var1);
+        let num2 = Number(var2);
+        switch (operation) {
+            case "+":
+                value = num1 + num2;
+                break;
+            case "-":
+                value = num1 - num2;
+                break;
+            case "*":
+                value = num1 * num2;
+                break;
+            case "%":
+                value = num1 / num2;
+                break;
+        }
+        display.textContent = value;
+    }
 }
