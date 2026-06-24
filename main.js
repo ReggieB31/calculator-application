9964b1e4d4552d75f6b7200bef965c815b46a1a0
Reginald BIllings <billingsreginald3@gmail.com>
2026-06-23 20:18:39 -0400

    git push -A

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-let input = "";
+let inputDisplay = "";
 let display = document.getElementById("input");
+let var1;
+let var2;
+let counter = 0;
+let operation;
+
 function calculate(e){
-    input += e.target.dataset.value;
-    display.textContent = input;
+    if(counter == 0 && !NaN(e.target.dataset.value)){
+        let var1 = e.target.dataset.value;
+        counter++; 
+    } else if(counter == 1 && !NaN(e.target.dataset.value)) {
+        var2 = e.target.dataset.value;
+        counter++;
+    } 
+    
+    if(counter == 0 || operation != undefined){
+        
+    }
+    inputDisplay += e.target.dataset.value;
+    display.textContent = inputDisplay;
+    
 }
