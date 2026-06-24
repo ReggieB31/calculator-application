13648660f50737eed1dda1d46cce278b119f1d64
Reginald BIllings <billingsreginald3@gmail.com>
2026-06-24 09:18:00 -0400

    git push -A

--- main.js
+++ main.js
@@ -8,12 +8,14 @@ let operation;
 
 function calculate(e){
     let inputValue = e.target.dataset.value;
+    // assigns input values to the two variables and displays them.
     if(counter == 0 && !isNaN(inputValue)){
         var1 += inputValue; 
     } else if(counter == 1 && !isNaN(inputValue)) {
         var2 += inputValue;
     } 
     
+    // Handles any operation besides clear.
     if(isNaN(inputValue) && inputValue != "clear"){
         if(operationCounter < 1){
         operation = inputValue;
@@ -27,7 +29,8 @@ function calculate(e){
     if(counter < 2 && !isNaN(inputValue)){
         inputDisplay += inputValue;
         display.textContent = inputDisplay;
-    } 
+    }
+    // Handles the equals operation and resets the equation with the result as the first variable.  
     if(inputValue == "="){
         let value;
         let num1 = Number(var1);
@@ -45,7 +48,27 @@ function calculate(e){
             case "%":
                 value = num1 / num2;
                 break;
+            case "^":
+                value = Math.pow(num1, num2);
+                break;
+        } 
+        inputDisplay = value;
+        display.textContent = inputDisplay;
+        var1 = value;
+        var2 = "";
+        counter = 0;
+        operationCounter = 0;
+        operation = undefined;
         }
-        display.textContent = value;
+
+    // Handles the clear button behavior.
+    if (inputValue == "clear"){
+            inputDisplay = "";
+            var1 = "";
+            var2 = "";
+            counter = 0;
+            operationCounter = 0;
+            operation = "";
+            display.textContent = inputDisplay;
     }
 }
